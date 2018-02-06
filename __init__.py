9a8b3116497eb07f343b20e14a77db1010d7b66c
Daniel Woschée <daniel.woschee@physik.lmu.de>
2018-02-06 15:40:00 +0100

    Started with framework and API

---
+++ __init__.py
@@ -0,0 +1,31 @@
+#! /bin/env python3
+
+
+# Define meta information
+__version__ = "0.0.1"
+__author__ = "Daniel Woschée"
+__contact__ = "daniel.woschee@physik.lmu.de"
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    import tkinter as tk
+    from src.module_manager import ModuleManager
+    from src.workflow import WorkflowGUI
+
+    # Add directories for builtins and plugins to PYTHONPATH
+    this_dir, _ = os.path.split(os.path.abspath(__file__))
+    SRC_PATH = os.path.join(this_dir, "src")
+    PLUGINS_PATH = os.path.join(this_dir, "plugins")
+
+    sys.path.append(SRC_PATH)
+    sys.path.append(PLUGINS_PATH)
+
+    # Load modules
+    modman = ModuleManager(PLUGINS_PATH)
+    modman.show()
+
+    # Display GUI
+    gui = WorkflowGUI(modman)
+    gui.mainloop()
---
+++ src/workflow.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+import tkinter as tk
+import tkinter.ttk as ttk
+
+PAD_X = 10
+PAD_Y = 10
+
+class WorkflowGUI:
+    def __init__(self, module_manager=None):
+        """Set up the workflow GUI."""
+        self.modman = module_manager
+        self.root = tk.Tk()
+        self.root.minsize(width=300, height=700)
+
+        self.mainframe = ttk.Frame(self.root)
+        self.mainframe.pack(fill=tk.BOTH, expand=True)
+
+        self.add_button = ttk.Button(self.mainframe, text="New", command=self.new_module)
+        self.add_button.pack(side=tk.TOP, fill=tk.X, padx=PAD_X, pady=PAD_Y)
+
+
+    def new_module(self):
+        self.add_button.pack_forget()
+        m = ModuleFrame(self.mainframe)
+        #m.pack(fill=tk.X, padx=PAD_X, pady=PAD_Y)
+        self.add_button.pack(fill=tk.X, padx=PAD_X, pady=PAD_Y)
+        
+
+
+    def mainloop(self):
+        """Start the root mainloop."""
+        self.root.mainloop()
+
+
+class ModuleFrame:
+    def __init__(self, parent):
+        self.parent = parent
+
+        self.frame = ttk.Frame(self.parent, relief=tk.RAISED,
+            borderwidth=2, width=250, height=150)
+        ttk.Label(self.frame, text="FRAME!").pack()
+        self.frame.pack(fill=tk.X, padx=PAD_X, pady=PAD_Y)
