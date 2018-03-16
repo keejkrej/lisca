7e125f8b8be1df302fd24485eeb80ab05e9d4d83
Daniel Woschée <daniel.woschee@physik.lmu.de>
2018-03-16 15:42:30 +0100

    Custom color scaling function for stack

--- src/stack.py
+++ src/stack.py
@@ -174,9 +174,29 @@ class Stack:
         return self.img[channel, frame, :, :].copy()
 
 
-    def get_frame_tk(self, channel, frame):
-        """Get a frame of the stack as Tk.PhotoImage."""
-        if self._mode == 8:
+    def get_frame_tk(self, channel, frame, convert_fcn=None):
+        """
+        Get a frame of the stack as :py:class:`tkinter.PhotoImage`.
+
+        :param channel: The channel of the requested stack position
+        :type channel: int
+        :param frame: The frame of the requested stack position
+        :type frame: int
+        :param convert_fcn: Custom conversion function
+        :type convert_fcn: None or function
+
+        If a custom conversion function is given, the function must take
+        one argument, which is a (n_rows, n_columns)-shaped numpy array
+        of the current stack position with the bit-depth of the original
+        image (typically 8 or 16 bit per pixel), and must return
+        a (n_rows, n_columns)-shaped numpy array of ``uint8`` type.
+
+        :return: the image at the requested stack position
+        :rtype: :py:class:`tkinter.PhotoImage`
+        """
+        if convert_fcn:
+            a8 = convert_fcn(self.get_image(channel, frame))
+        elif self._mode == 8:
             a8 = self.get_image(channel, frame)
         else:
             a16 = self.get_image(channel, frame)
