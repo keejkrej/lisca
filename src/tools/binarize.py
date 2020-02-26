074bbaff8e192266c455e37e059c13264ab8a289
Daniel Woschée <daniel.woschee@physik.lmu.de>
2020-02-26 18:52:48 +0100

    More convenient binarization and bbox export

--- src/tools/binarize.py
+++ src/tools/binarize.py
@@ -10,14 +10,14 @@ def binarize_phasecontrast_stack(stack, i_channel, outfile, status=None):
     if status is None:
         status = DummyStatus()
 
-    stack_bin = np.empty((stack.n_frames, stack.height, stack.width), dtype=np.bool_)
+    stack_bin = np.empty((stack.n_frames, 1, 1, stack.height, stack.width), dtype=np.uint8)
     with status("Binarizing …") as current_status:
         for i_frame in range(stack.n_frames):
             current_status.reset(msg="Binarizing frame", current=i_frame+1, total=stack.n_frames)
             stack_bin[i_frame, ...] = binarize_frame(stack.get_image(frame=i_frame, channel=i_channel))
 
         current_status.reset(f"Saving binarized stack to '{outfile}' …")
-        tiff.imwrite(outfile, stack_bin)
+        tiff.imwrite(outfile, stack_bin, imagej=True)
 
         current_status.reset(f"Saved binarized stack to '{outfile}'.")
         time.sleep(2)
--- src/tools/roi_bboxer.py
+++ src/tools/roi_bboxer.py
@@ -3,11 +3,16 @@ import numpy as np
 
 def get_selected_bboxes(session, filename):
     """Create a dict of bboxes of all cells"""
-    selected_traces = {}
+    bboxes = {}
     with session.lock:
+        bboxes[None] = dict(n_frames=session.stack.n_frames,
+                            width=session.stack.width,
+                            height=session.stack.height,
+                           )
         for name, tr in session.traces.items():
             if not tr['select']:
                 continue
+            bboxes[name] = {}
             x_min = None
             x_max = None
             y_min = None
@@ -22,8 +27,9 @@ def get_selected_bboxes(session, filename):
                     y_min = bb.y_min
                 if y_max is None or bb.y_max > y_max:
                     y_max = bb.y_max
-            selected_traces[name] = dict(x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max)
+                bboxes[name][fr] = dict(x_min=bb.x_min, x_max=bb.x_max, y_min=bb.y_min, y_max=bb.y_max)
+            bboxes[name][...] = dict(x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max)
     with open(filename, 'wb') as f:
-        pickle.dump(selected_traces, f)
-    print(f"{len(selected_traces)} bounding boxes written to {filename}")
+        pickle.dump(bboxes, f)
+    print(f"{len(bboxes)-1} bounding boxes written to {filename}")
 
